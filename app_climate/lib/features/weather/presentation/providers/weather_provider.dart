1ff814bb2a438e6ef8a2ea79c989e27994d2463b
Lobosanplay <parra.santiago2008@gmail.com>
2025-08-16 17:45:54 -0500

    cambios menores

--- app_climate/lib/features/weather/presentation/providers/weather_provider.dart
+++ app_climate/lib/features/weather/presentation/providers/weather_provider.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart' ;
 
-
-
 class WeatherProvider extends ChangeNotifier{
   bool _isLoading = false;
   Map<String, dynamic>? _weatherData = {};
--- app_climate/test/widget_test.dart
+++ app_climate/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_climate/main.dart';
+import 'package:app_climate/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const climate());
+    await tester.pumpWidget(const ClimateApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
